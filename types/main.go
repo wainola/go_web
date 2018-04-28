463da3afe26754db6a148d1735ee9c3bf7f48021
Nicolas Riquelme Guzman <nrriquelme@gmail.com>
2018-04-27 23:19:28 -0300

    structs and collections

--- types/main.go
+++ types/main.go
@@ -13,6 +13,9 @@ type Persona struct {
 	Added    time.Time
 }
 
+type Cuenta struct {
+}
+
 func main() {
 	// Instanciado tipos
 	var p Persona
@@ -25,13 +28,35 @@ func main() {
 	fmt.Println(p)
 
 	// Coleccion de tipos.
-	curso := []Persona{}
+	curso := make([]Persona, 3)
 	curso[0] = Persona{
-		Nombre: "Camilo",
+		Nombre:   "Camilo",
 		Apellido: "Riquelme",
-		Email: "camilo@mail.com",
-		Edad: 28,
-		Added: time.Date(2018,04,27,0,0,0,0,time.UTC)
+		Email:    "camilo@mail.com",
+		Edad:     28,
+		Added:    time.Date(2018, 04, 27, 0, 0, 0, 0, time.UTC),
 	}
-	curso[1] = 
+	curso[1] = Persona{
+		Nombre:   "Sylvia",
+		Apellido: "Guzman",
+		Email:    "olmue2@gmail.com",
+		Edad:     56,
+		Added:    time.Date(2018, 04, 27, 0, 0, 0, 0, time.UTC),
+	}
+
+	curso[2] = Persona{
+		Nombre:   "Jorge",
+		Apellido: "Aliaga",
+		Email:    "jorge@gmail.com",
+		Edad:     55,
+		Added:    time.Date(2018, 04, 27, 0, 0, 0, 0, time.UTC),
+	}
+
+	for _, v := range curso {
+		if v.Nombre == "Nicolas" {
+			fmt.Println("Hay un Nicolas en el slice!")
+		} else {
+			fmt.Println("No hay Nicolas en el slice")
+		}
+	}
 }
